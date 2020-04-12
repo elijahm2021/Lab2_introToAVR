791d502ae86ab5919b73d89c003aaa62a283089f
Elijah McIntosh <emcin002@wch136-24.cs.ucr.edu>
2020-04-12 16:48:26 -0700

    Added configurations for pins

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: emcin002
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 027
+ *	Assignment: Lab #2  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -14,10 +14,11 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
     /* Insert your solution below */
     while (1) {
-
+		
     }
     return 1;
 }
