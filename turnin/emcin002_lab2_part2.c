5256f1b4620babbb1122241c428650c10b2760b5
Elijah McIntosh <emcin002@wch136-24.cs.ucr.edu>
2020-04-13 19:07:09 -0700

    Completed part 2

---
+++ turnin/emcin002_lab2_part2.c
@@ -0,0 +1,52 @@
+/*	Author: emcin002
+ *  Partner(s) Name: 
+ *	Lab Section: 027
+ *	Assignment: Lab #2  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum States {Start, PARKING} state;
+unsigned char tmpA = 0x00;
+unsigned char tmpC = 0x00;
+unsigned char cntavail = 0x00;
+
+void Tick() {
+	switch(state) {
+		case Start:
+			state = PARKING;
+			break;
+		case PARKING:	
+			break;
+		default:
+			break;
+	}
+	switch(state) {	
+		case Start:
+			break;
+		case PARKING:
+			cntavail = 0x0F - tmpA;
+			tmpC = cntavail;
+			break;
+		default:
+			break;
+	}
+}
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	state = Start;
+	while(1) {
+		tmpA = PINA;
+		Tick();
+		PORTC = tmpC;
+	}
+    return 1;
+}
